c9c0156684b380d40f5fafb0016d508b2556c3a0
Will Holmes <willholmes60@gmail.com>
2021-11-10 16:55:00 +0000

    styling to do

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,12 +10,20 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
   end
 
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
-
   end
 end
